228bb253007c3be3e8b7bcd86a1d39f9290c69e6
Legat <zia-d@mail.ru>
2022-04-06 21:48:46 +0400

    feat: add hover effects to links and buttons

--- shelter/assets/pages/main/script.js
+++ shelter/assets/pages/main/script.js
@@ -6,3 +6,11 @@ const ourFriendsSection = document.querySelector('.our-friends');
 makeFriendBtn.addEventListener('click', () => {
   ourFriendsSection.scrollIntoView(true);
 });
+
+//------Carousel button link------
+
+const GetToKnowBtn = document.querySelector('.our-friends__button');
+
+GetToKnowBtn.addEventListener('click', () => {
+  document.location.href = "./our-pats.html";
+});
